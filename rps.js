238896a2e91bf69e641c616ad5ea42f214b2f5cd
kendallini <scace22@gmail.com>
2022-03-24 16:22:00 -0700

    score board working rps.js

--- rps.js
+++ rps.js
@@ -5,36 +5,64 @@ let options = ['rock','paper', 'scissors'];
  }
 const computerSelection = computerPlay();
 
-
+let playerScore = 0;
+let computerScore = 0;
 
 function playRound(playerSelection, computerSelection) {
+    
     if (playerSelection == computerSelection) {
         return 'Tie.'
      } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
+        playerScore ++;
     return 'You win! Rock beats scissors';
     } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
+        computerScore ++;
         return 'You lose! Scissors beats paper.';
     } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
+        playerScore ++;
         return 'You win! Scissors beats paper.';
     } else if (playerSelection == 'rock' && computerSelection == 'paper') {
+        computerScore ++;
          return 'You lose! Paper beats rock.';
      } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
+        computerScore ++;
          return 'You lose! Rock beats scissors';
      } else if (playerSelection == 'paper' && computerSelection == 'rock') {
+        playerScore ++;
          return 'You win! Paper beats rock.';
      }
     }
-  //  console.log(playRound(playerSelection,computerSelection));
+  
 
-
+  let playerSelection = undefined;
    function game() {
+    
+    playerSelection;
+    playRound(playerSelection, computerSelection);
         for (let i=0; i < 5; i++ ){
-          
-          let playerSelection = prompt('Select rock, paper, scissors.');
-        playRound(playerSelection, computerSelection);
-        if (playerSelection ++);
-           else (computerSelection ++);}
+        
+        let playerSelection = prompt('Select rock, paper, scissors.');
+        computerPlay();
+        console.log(playRound(playerSelection,computerSelection));
+        console.log('you', playerScore);
+        console.log('computer', computerScore);
+        console.log(computerSelection)
+     
+        /*if (playerScore >= 5) {
+            return 'Winner!';
+        } else if (computerScore == 5){
+               return 'ur a fkn loser.'; 
+           } else (playerScore <5 || computerScore <5) ;
+                return "ya'll suck";
+           }*/
+        }
     }
+        
+               
+           
+           
+    
+           
 
     /*
     if (question.toLowerCase = options) {
